41bb150ccb861f1768d447f667209294c9303473
thiramoss <trs.tiba@gmail.com>
2024-07-10 00:50:36 -0300

    Persistindo dados no banco

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -26,7 +26,7 @@ public class Serie {
         private String poster;
         private String sinopse;
 
-        @Transient
+        @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
         private List<Episodio> episodios = new ArrayList<>();
 
         public Serie(){}
@@ -46,6 +46,7 @@ public class Serie {
         }
 
         public void setEpisodios(List<Episodio> episodios) {
+                episodios.forEach(e -> e.setSerie(this));
                 this.episodios = episodios;
         }
 
@@ -122,6 +123,7 @@ public class Serie {
                         ", atores='" + atores + '\'' +
                         ", poster='" + poster + '\'' +
                         ", sinopse='" + sinopse + '\'' +
+                        ", sinopse='" + episodios + '\'' +
                         '}';
         }
 }
