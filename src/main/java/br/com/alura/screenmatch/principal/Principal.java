be2bf6e701dee50fe8b34fd92cb5295f561228a9
thiramoss <trs.tiba@gmail.com>
2024-07-10 23:22:27 -0300

    Trabalhando com derived queries

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -38,7 +38,9 @@ public class Principal {
                     1 - Buscar séries
                     2 - Buscar episódios
                     3 - Listar séries buscadas 
-                    4 - Buscar série por título    
+                    4 - Buscar séries por título
+                    5 - Buscar séries por ator
+                    6 - Top 5 séries
                     0 - Sair                                 
                     """;
 
@@ -59,6 +61,11 @@ public class Principal {
                 case 4:
                     buscarSeriePorTitulo();
                     break;
+                case 5:
+                    buscarSeriesPorAtor();
+                    break;
+                case 6:
+                    buscaTop5Series();
                 case 0:
                     System.out.println("Saindo...");
                     break;
@@ -67,6 +74,7 @@ public class Principal {
             }
         }
     }
+
 
 
 
@@ -138,5 +146,22 @@ public class Principal {
         } else {
             System.out.println("Série não encontrada");
         }
+    }
+
+    private void buscarSeriesPorAtor() {
+        System.out.println("Qual o nome para busca?");
+        var nomeAtor = leitura.nextLine();
+        System.out.println("Avaliações a partir de qual valor?");
+        var avaliacao = leitura.nextDouble();
+        List<Serie> seriesEncontradas = repositorio.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, avaliacao);
+        System.out.println("Séries em que " + nomeAtor + "trabalhou: ");
+        seriesEncontradas.forEach(s ->
+                System.out.println(s.getTitulo() + " Avalição: " + s.getAvaliacao()));
+    }
+
+    private void buscaTop5Series() {
+        List<Serie> serieTop = repositorio.findTop5ByOrderByAvaliacaoDesc();
+        serieTop.forEach(s ->
+                System.out.println(s.getTitulo() + " Avalição: " + s.getAvaliacao()));
     }
 }
